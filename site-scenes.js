687730564c6a9713b077d68d5675ddb360cea8ea
Josh Randall <188399963+joshprandall@users.noreply.github.com>
2026-09-24 13:14:22 -0700

    Refresh capped site audio controller

--- site-scenes.js
+++ site-scenes.js
@@ -17,7 +17,7 @@
     window.__JR_SITE_AUDIO_LOADING__ = true;
     const script = document.createElement('script');
     script.id = 'jr-site-audio-controller';
-    script.src = new URL('site-audio.js?v=20260924-unified-v14', SITE_BASE).href;
+    script.src = new URL('site-audio.js?v=20260924-unified-v15', SITE_BASE).href;
     script.async = false;
     script.onload = () => {
       window.__JR_SITE_AUDIO_LOADING__ = false;
