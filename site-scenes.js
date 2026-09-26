ba095d6baeb70a093c09399d3e55c145a7e50935
Josh Randall <188399963+joshprandall@users.noreply.github.com>
2026-09-25 19:41:50 -0700

    -- Refresh shared audio architecture loader

--- site-scenes.js
+++ site-scenes.js
@@ -17,7 +17,7 @@
     window.__JR_SITE_AUDIO_LOADING__ = true;
     const script = document.createElement('script');
     script.id = 'jr-site-audio-controller';
-    script.src = new URL('site-audio.js?v=20260925-main-autoplay-v26', SITE_BASE).href;
+    script.src = new URL('site-audio.js?v=20260925-architecture-v28', SITE_BASE).href;
     script.async = false;
     script.onload = () => {
       window.__JR_SITE_AUDIO_LOADING__ = false;
